6361b602f81ee4b7bea7910a20cfd4cf06779a75
Sania2001 <saniagshinde@gmail.com>
2022-03-15 22:49:53 +0530

    Unfinished

---
+++ src/com/poly/Add.java
@@ -0,0 +1,22 @@
+package com.poly;
+
+import java.util.Scanner;
+
+
+    public static void main(String[] args) {
+public class Add{
+    Scanner input = new Scanner(System.in);
+    int x,y,z,r;
+    public int AdditionOperation(int x, int y)
+    {
+        int r= x+y;
+        return r;
+    }
+    public int AdditionOperation(int x, int y, int z)
+    {
+        int r = x + y + z;
+        return r;
+    }
+    }
+
+
---
+++ src/com/poly/Polyy.java
@@ -0,0 +1,18 @@
+package com.poly;
+
+import java.util.Scanner;
+
+public class Polyy {
+    public int AdditionOperation(int x, int y)
+    {
+        return x+y;
+    }
+    public int AdditionOperation(int x, int y, int z)
+    {
+       return x +y +z;
+    }
+
+    public static void main(String[] args) {
+
+    }
+    }
